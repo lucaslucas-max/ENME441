3e5be01cd7cd8faaf477d1bc66d9aa1d9de0e47d
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-07 21:44:02 -0400

    Create Lab5a.py

---
+++ Lab5a.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import math
+import time 
+
+f = 0.2
+start = time.time()
+
+GPIO.setmode(GPIO.BCM)
+p=4
+GPIO.setup(p, GPIO.OUT)
+
+pwm = GPIO.PWM(p, 500)
+pwm.start(0)
+
+try:
+	while True:
+		t = time.time() - start
+		B = (math.sin(2*math.pi*f*t))^2
+		duty = brightness * 100
+		pwm.ChangeDutyCycle(duty)
+
+except KeyboardInterrupt:
+	pass
+
+pwm.stop()
+GPIO.cleanup()
