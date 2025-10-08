5db50fc38ecf24a5c1b0694c3ad4ddb7cbc58178
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-07 21:52:26 -0400

    Update Lab5a.py

--- Lab5a.py
+++ Lab5a.py
@@ -15,7 +15,7 @@ pwm.start(0)
 try:
 	while True:
 		t = time.time() - start
-		B = (math.sin(2*math.pi*f*t))^2
+		B = (math.sin(2*math.pi*f*t))**2
 		duty = brightness * 100
 		pwm.ChangeDutyCycle(duty)
 
