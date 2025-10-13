615af13b76deb0ad1fc87025ddaf5f4996fa3a99
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-13 15:05:06 -0400

    Update Lab6a.py

--- Lab6a.py
+++ Lab6a.py
@@ -6,19 +6,17 @@ GPIO.setup(dataPin, GPIO.OUT)
 GPIO.setup(latchPin, GPIO.OUT, initial=0) # start latch & clock low
 GPIO.setup(clockPin, GPIO.OUT, initial=0)
 pattern = 0b01100110 # pattern to display
-def ping(p): # ping the clock or latch pin
-  GPIO.output(p,1)
+for i in range(8):
+  GPIO.output(dataPin, pattern & (1<<i))
+  GPIO.output(clockPin,1) # ping the clock pin to shift register data
   time.sleep(0)
-  GPIO.output(p,0)
-def shiftByte(b): # send a byte of data to the output
-  for i in range(8):
-    GPIO.output(dataPin, b & (1<<i))
-    ping(clockPin) # add bit to register
-  ping(latchPin) # send register to output
+  GPIO.output(clockPin,0)
+  
+GPIO.output(latchPin, 1) # ping the latch pin to send register to output
+time.sleep(0)
+GPIO.output(latchPin, 0)
 try:
   while 1:
-    for i in range(2**8):
-      shiftByte(i)
-      time.sleep(0.5)
+    pass
 except:
   GPIO.cleanup()
