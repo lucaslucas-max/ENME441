35934593583da393e7ee07b8f0cca9d011702a95
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-13 19:28:39 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -13,29 +13,14 @@ class Shifter:
         GPIO.setup(self.latchPin, GPIO.OUT, initial=GPIO.LOW)
     
     def _ping(self, pin):
-        """Private method to toggle a pin high then low."""
         GPIO.output(pin, GPIO.HIGH)
         time.sleep(0)
         GPIO.output(pin, GPIO.LOW)
     
     def shiftByte(self, data):
-        """Shift out one byte of data to the shift register."""
         for i in range(8):
             GPIO.output(self.serialPin, (data >> i) & 1)
             self._ping(self.clockPin)
         self._ping(self.latchPin)
 
-# --- Example usage ---
-if __name__ == "__main__":
-    try:
-        dataPin, latchPin, clockPin = 23, 24, 25
-        shifter = Shifter(dataPin, clockPin, latchPin)
-        
-        pattern = 0b01100110
-        shifter.shiftByte(pattern)
-        
-        # Keep running
-        while True:
-            pass
-    except KeyboardInterrupt:
         GPIO.cleanup()
