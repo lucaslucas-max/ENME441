a7b116eba09ff98e06a1bb4b1e8c40b89f768594
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-13 21:15:22 -0400

    Create bugclass.py

---
+++ bugclass.py
@@ -0,0 +1,42 @@
+import time
+import random
+import RPi.GPIO as GPIO
+from shifter import Shifter
+
+class Bug:
+  def __int__(self, timestep=0.1, x=3, isWrapOn= False)
+    self.timestep = timestep
+    self.x = x
+    self.isWrapOn = False
+
+    Data = 23
+    Latch = 24
+    Clock = 25
+    self.__Shifter = Shifter(Data, Clock, Latch)
+
+    self._running = False
+
+  def display(self):
+  pattern = 1 << self.x
+  self.__shifter.shiftByte(pattern)
+
+  def start(self):
+    self._running = True
+    try:
+      while self._running
+      self.__update_display()
+      time.sleep(self.timestep)
+      move = random.choice([-1, 1])
+      self.x += move
+      if self.x < 0:
+        self.x = 0
+      elif self.x > 7:
+        self.x = 7
+
+    except KeyboardInterrupt:
+      self.stop()
+
+def stop(self):
+  self._running = False
+  self.__shifter.shiftByte(0b00000000)
+  GPIO.cleanup()
