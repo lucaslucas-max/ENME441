18c1c7985e1198fe566e09b8ff2bad873f60bb13
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-19 21:28:58 -0400

    Update bugclass.py

--- bugclass.py
+++ bugclass.py
@@ -1,42 +1,58 @@
+# bugclass.py
 import time
 import random
 import RPi.GPIO as GPIO
 from shifter import Shifter
 
 class Bug:
-  def __init__(self, timestep=0.1, x=3, isWrapOn= False):
-    self.timestep = timestep
-    self.x = x
-    self.isWrapOn = False
+    def __init__(self, timestep=0.1, x=3, isWrapOn=False):
+        """
+        timestep: time step size in seconds
+        x: initial LED position (0–7)
+        isWrapOn: if True, LED wraps around edges
+        """
+        self.timestep = timestep
+        self.x = x
+        self.isWrapOn = isWrapOn
 
-    Data = 23
-    Latch = 24
-    Clock = 25
-    self.__Shifter = Shifter(Data, Clock, Latch)
+        # Private Shifter object for LED control
+        DATA, LATCH, CLOCK = 23, 24, 25
+        self.__shifter = Shifter(DATA, CLOCK, LATCH)
 
-    self._running = False
+        # Control flag for running loop
+        self._running = False
 
-  def display(self):
-    pattern = 1 << self.x
-    self.__shifter.shiftByte(pattern)
+    def __update_display(self):
+        """Private: update the LEDs to show the current position."""
+        pattern = 1 << self.x
+        self.__shifter.shiftByte(pattern)
 
-  def start(self):
-    self._running = True
-    try:
-      while self._running:
-        self.__update_display()
-        time.sleep(self.timestep)
-        move = random.choice([-1, 1])
-        self.x += move
-        if self.x < 0:
-          self.x = 0
-        elif self.x > 7:
-          self.x = 7
+    def start(self):
+        """Start moving the LED in a random walk."""
+        self._running = True
+        try:
+            while self._running:
+                self.__update_display()
+                time.sleep(self.timestep)
 
-    except KeyboardInterrupt:
-      self.stop()
+                # Move LED left or right randomly
+                move = random.choice([-1, 1])
+                self.x += move
 
-def stop(self):
-  self._running = False
-  self.__shifter.shiftByte(0b00000000)
-  GPIO.cleanup()
+                if self.isWrapOn:
+                    # Wrap around edges
+                    self.x %= 8
+                else:
+                    # Keep LED inside range
+                    if self.x < 0:
+                        self.x = 0
+                    elif self.x > 7:
+                        self.x = 7
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        """Stop movement and clear LEDs."""
+        self._running = False
+        self.__shifter.shiftByte(0b00000000)
+        GPIO.cleanup()
