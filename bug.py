30e768936f6f1a37936bd7ee9ac7d23858c2ce03
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-20 14:07:48 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,39 +1,34 @@
 import RPi.GPIO as GPIO
 import time
-from bugclass import Shifter, Bug
-
-# Setup GPIO
+from bugclass import shifter, Bug
 GPIO.setmode(GPIO.BCM)
+
 dataPin, latchPin, clockPin = 23, 24, 25
-s1, s2, s3 = 17, 27, 22  # Input switches
+s1, s2, s3 = 17, 27, 22 
 
 for pin in [s1, s2, s3]:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Create shifter and bug
-s = Shifter(dataPin, latchPin, clockPin)
+s = shifter(dataPin, latchPin, clockPin)
 bug = Bug(s)
 
 last_s2 = GPIO.input(s2)
 
 try:
     while True:
-        # a. Turn bug on/off with s1
+
         bug.running = GPIO.input(s1) == GPIO.HIGH
 
-        # b. Flip wrap state when s2 changes
         s2_state = GPIO.input(s2)
         if s2_state != last_s2:
             bug.isWrapOn = not bug.isWrapOn
             last_s2 = s2_state
 
-        # c. Increase speed when s3 is on (reduce timestep)
         if GPIO.input(s3) == GPIO.HIGH:
             bug.timestep = 0.1 / 3
         else:
             bug.timestep = 0.1
 
-        # move bug
         bug.move_once()
 
 except KeyboardInterrupt:
