b94354d7716efe82efdcdf2249b696d2c7f25910
lucaslucas-max <lucas.j.rose04@gmail.com>
2025-10-28 02:19:57 -0400

    Update servertest.py

--- servertest.py
+++ servertest.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer
 from http.server import SimpleHTTPRequestHandler
 # Serve files from current directory
-server_address = ('127.0.0.1', 8080)
+server_address = ('127.0.0.1', 8000)
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-print("Serving on port 8080...")
+print("Serving on port 8000...")
 httpd.serve_forever()
